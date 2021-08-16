c1f60e115567a92687bc21830ba10b6ef6a22dc1
Layl Bongers <2385329-layl@users.noreply.gitlab.com>
2021-08-16 20:10:12 +0200

    Fix name casing

--- src/test/scala/nanoben/NanoBenTopLevelTests.scala
+++ src/test/scala/nanoben/NanoBenTopLevelTests.scala
@@ -12,42 +12,44 @@ object NanoBenTopLevelTests {
       .withWave
       .compile(new NanoBenTopLevel)
 
-    compiled.doSim { dut =>
-      dut.clockDomain.forkStimulus(period = 10)
-      val r = new Random(1234)
+    compiled.doSim(testRegisters)
+  }
 
-      for (_ <- 1 to 10) {
-        val value_0 = r.nextInt(256)
-        val value_1 = r.nextInt(256)
+  def testRegisters(dut: NanoBenTopLevel): Unit = {
+    dut.clockDomain.forkStimulus(period = 10)
+    val r = new Random(1234)
 
-        // Store value in register 0
-        dut.io.sw #= switches(value_0, write_0 = true)
-        dut.clockDomain.waitRisingEdge()
+    for (_ <- 1 to 10) {
+      val value0 = r.nextInt(256)
+      val value1 = r.nextInt(256)
 
-        // Store value in register 1
-        dut.io.sw #= switches(value_1, write_1 = true)
-        dut.clockDomain.waitRisingEdge()
+      // Store value in register 0
+      dut.io.sw #= switches(value0, write0 = true)
+      dut.clockDomain.waitRisingEdge()
 
-        // Retrieve and check value in register 0
-        dut.io.sw #= switches(select = 1)
-        dut.clockDomain.waitRisingEdge()
-        var result = dut.io.led.toInt
-        assert(result == value_0, s"Got $result expected $value_0")
+      // Store value in register 1
+      dut.io.sw #= switches(value1, write1 = true)
+      dut.clockDomain.waitRisingEdge()
 
-        // Retrieve and check value in register 1
-        dut.io.sw #= switches(select = 2)
-        dut.clockDomain.waitRisingEdge()
-        result = dut.io.led.toInt
-        assert(result == value_1, s"Got $result expected $value_1")
-      }
+      // Retrieve and check value in register 0
+      dut.io.sw #= switches(select = 1)
+      dut.clockDomain.waitRisingEdge()
+      var result = dut.io.led.toInt
+      assert(result == value0, s"Got $result expected $value0")
+
+      // Retrieve and check value in register 1
+      dut.io.sw #= switches(select = 2)
+      dut.clockDomain.waitRisingEdge()
+      result = dut.io.led.toInt
+      assert(result == value1, s"Got $result expected $value1")
     }
   }
 
-  def switches(bus: Int = 0, select: Int = 0, write_0: Boolean = false, write_1: Boolean = false): Int = {
+  def switches(bus: Int = 0, select: Int = 0, write0: Boolean = false, write1: Boolean = false): Int = {
     var switches = bus
     switches |= select << 8
-    switches |= write_0.toInt << 11
-    switches |= write_1.toInt << 10
+    switches |= write0.toInt << 11
+    switches |= write1.toInt << 10
     switches
   }
 }
