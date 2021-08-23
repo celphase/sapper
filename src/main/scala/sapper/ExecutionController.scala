0350c81b263570d74aedf6a453775abd1f9a5826
Layl Bongers <2385329-layl@users.noreply.gitlab.com>
2021-08-23 22:28:39 +0200

    Misc corrections and fixes to the execution controller

--- src/main/scala/sapper/ExecutionController.scala
+++ src/main/scala/sapper/ExecutionController.scala
@@ -13,47 +13,62 @@ case class ExecutionController() extends Component {
   }
 
   // Microcode ROM
-  val MC_BUS_SELECT_MEMORY = "0_0000_0011" b
-  val MC_BUS_SELECT_PROGRAM_COUNTER = "0_0000_0100" b
-  val MC_WRITE_ADDRESS = "0_0100_0000" b
-  val MC_STORE_INSTRUCTION = "1_0000_0000" b
+  // Instruction: reserved, alu mode, 3 bits STORE, 3 bits SELECT
+  // All instructions end with fetching a new instruction and resetting the micro step counter
+  // 8 slots of microcode per instruction
+  val MC_BUS_READ_MEMORY = "0000_0011" b
+  val MC_BUS_READ_PROGRAM_COUNTER = "0000_0100" b
+  val MC_BUS_WRITE_ADDRESS = "0001_1000" b
+  val MC_BUS_WRITE_INSTRUCTION = "0010_1000" b
   val microcode = Mem(
     Bits(9 bits),
     Seq(
       // Get the memory to read the instruction
-      MC_BUS_SELECT_PROGRAM_COUNTER | MC_WRITE_ADDRESS,
+      MC_BUS_READ_PROGRAM_COUNTER | MC_BUS_WRITE_ADDRESS,
       // Instruction should now be on the bus, bring it over to the execution controller
-      MC_BUS_SELECT_MEMORY | MC_STORE_INSTRUCTION
+      MC_BUS_READ_MEMORY | MC_BUS_WRITE_INSTRUCTION
     ).map { v => B(v) }
   )
 
   // Program counter
   val programCounter = Reg(UInt(8 bits)) init 0
-  //programCounter := programCounter + 1
   io.outProgramCounter := programCounter
 
   // Instruction register
   val storeInstruction = Bool()
   val instruction = RegNextWhen(io.inBus(3 downto 0), storeInstruction) init 0
 
-  // If the instruction is stored, we also want to jump forward
-  when(storeInstruction.rise) {
-    programCounter := programCounter + 1
-  }
-
   // Microcode sub-steps
-  val microStep = Reg(UInt(4 bits)) init 0
+  val microStep = Reg(UInt(3 bits)) init 0
   microStep := microStep + 1
 
-  // Drive the control signals
+  // Fetch the micro-instruction from the ROM
   val microAddress = B(instruction, microStep).asUInt
   val trimmedAddress = microAddress(0).asUInt
   val signalBits = microcode.readAsync(trimmedAddress)
-  io.outSignals.select := signalBits(2 downto 0).asUInt
-  io.outSignals.writeRegister0 := signalBits(3)
-  io.outSignals.writeRegister1 := signalBits(4)
-  io.outSignals.aluMode := signalBits(5)
-  io.outSignals.writeAddressRegister := signalBits(6)
-  io.outSignals.writeMemory := signalBits(7)
-  storeInstruction := signalBits(8)
+
+  // Drive the signals for this micro-instruction
+  val busSelect = signalBits(2 downto 0).asUInt
+  io.outSignals.select := busSelect
+
+  io.outSignals.writeRegister0 := False
+  io.outSignals.writeRegister1 := False
+  io.outSignals.writeAddressRegister := False
+  io.outSignals.writeMemory := False
+  storeInstruction := False
+  switch(signalBits(5 downto 3)) {
+    // 0 is intentionally left no-op
+    is(1)(io.outSignals.writeRegister0 := True)
+    is(2)(io.outSignals.writeRegister1 := True)
+    is(3)(io.outSignals.writeAddressRegister := True)
+    is(4)(io.outSignals.writeMemory := True)
+    is(5)(storeInstruction := True)
+  }
+
+  io.outSignals.aluMode := signalBits(6)
+
+  // Writing the program counter to the bus will increment it on the next cycle
+  when(busSelect === 4) {
+    programCounter := programCounter + 1
+  }
 }
